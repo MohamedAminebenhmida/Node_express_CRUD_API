74707865bd9bfedc0fe2892b061a2a4836585dfd
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2023-02-20 19:09:12 +0100

    CRUD operations

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express=require('express');
 const  uuid =require('uuid');
 const uuid4=uuid.v4();
 const router=express.Router();
-const users=[
+let users=[
 
 ]
 
@@ -15,5 +15,15 @@ router.post('/',(req,res)=>{
     users.push({...user,id:uuid4})
    res.send('user created with success!') 
 })
+router.get('/:id',(req,res)=>{
 
+    const {id}=req.params;
+    const foundUser=users.find((user)=>user.id===id)
+    res.send(foundUser)
+})
+router.delete('/:id',(req,res)=>{
+    const {id}=req.params;
+    users=users.filter((user)=>user.id!==id);
+    res.send(`user with id ${id} was deleted with sucess`)
+})
 module.exports = router;
